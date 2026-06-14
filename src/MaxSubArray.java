3ff36c763193623c2694eb76446c7cbc55f60bd6
Vidushi <vidushisharma071@gmail.com>
2026-06-14 16:28:28 +0530

    Max subarray Sum

---
+++ src/MaxSubArray.java
@@ -0,0 +1,44 @@
+public class MaxSubArray {
+
+    // TC: O(N^2) SC: O(1)
+    public int maxSubArray( int[] nums){
+        int sum;
+        int max = Integer. MIN_VALUE;
+        for(int i = 0; i < nums.length; i++){
+            sum = 0;
+            for(int j = i; j < nums.length; j++){
+                sum += nums[j];
+                max = Math.max(sum, max);
+            }
+
+        }
+        return max;
+    }
+
+    // Kadane Algorithm
+    // TC: O(N) SC: O(1)
+    public int  maxSubArray2(int[] nums){
+        int max = Integer. MIN_VALUE;
+        int sum =0;
+        for(int i =0;i<nums.length;i++){
+
+            sum+= nums[i];
+            if(sum < 0)
+                sum = 0;
+
+            max = Math.max(sum, max);
+
+        }
+        return max;
+    }
+
+
+
+    public static void main() {
+        MaxSubArray obj = new MaxSubArray();
+        int ans = obj.maxSubArray(new int[]{2, 3, 5, -2, 7, -4});
+        int ans2 = obj.maxSubArray2(new int[]{2, 3, 5, -2, 7, -4});
+        System.out.println(ans);
+        System.out.println(ans2);
+    }
+}
