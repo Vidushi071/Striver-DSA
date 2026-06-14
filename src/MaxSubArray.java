7aa9cefd05f941af213e1e86e9a22b8090d7c410
Vidushi <vidushisharma071@gmail.com>
2026-06-14 16:38:30 +0530

    Print the Max subarray elements

--- src/MaxSubArray.java
+++ src/MaxSubArray.java
@@ -32,13 +32,45 @@ public class MaxSubArray {
         return max;
     }
 
+    public void maxSubArray3(int[] nums){
+        int max = Integer. MIN_VALUE;
+        int sum =0;
+        int start = 0;
+        int startIndex = -1;
+        int endIndex = -1;
 
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+
+            if(sum == 0)
+            {
+                start = i;
+            }
+            if(sum > max)
+            {
+                max = sum;
+                startIndex = start;
+                endIndex = i;
+            }
+
+            if(sum < 0)
+            {
+                sum = 0;
+            }
+        }
+
+        for(int i = startIndex; i <= endIndex; i++){
+            System.out.print(nums[i]+" ");
+        }
+
+    }
 
     public static void main() {
         MaxSubArray obj = new MaxSubArray();
         int ans = obj.maxSubArray(new int[]{2, 3, 5, -2, 7, -4});
         int ans2 = obj.maxSubArray2(new int[]{2, 3, 5, -2, 7, -4});
-        System.out.println(ans);
-        System.out.println(ans2);
+        obj.maxSubArray3(new int[]{2, 3, 5, -2, 7, -4});
+        //System.out.println(ans);
+       // System.out.println(ans2);
     }
 }
